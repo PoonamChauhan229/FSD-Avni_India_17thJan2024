d45d77e7b9ab2eb4ef6f2c8a9fff0691afdfea55
PoonamChauhan229 <poonam.chauhan229@gmail.com>
2024-03-20 19:19:02 +0530

    props passing

--- 11reactproject/src/App.jsx
+++ 11reactproject/src/App.jsx
@@ -5,21 +5,32 @@ function App() {
 
   return (
     <>
-     <h1>Hello World!!</h1>
+    <div className='App'>
+     {/* <h1>Hello World!!</h1> */}
 
      {/* React */}
      {/* JS  not able to use it >args || params  not able to use it directly*/}
-     {ProfileCard()}
+     {/* {ProfileCard()} */}
 
      {/* REact 1 way */}
-     <ProfileCard></ProfileCard>
+     {/* <ProfileCard></ProfileCard> */}
 
      {/* React 2 way most used onne */}
-     <ProfileCard/> 
-     <ProfileCard/> 
-     <ProfileCard/> 
-     <ProfileCard/> 
+     <ProfileCard image={"https://www.pngitem.com/pimgs/m/404-4042710_circle-profile-picture-png-transparent-png.png"} name={"Salman"} nickname="Bhai"/> 
 
+     <ProfileCard image={"https://www.pngitem.com/pimgs/m/404-4042710_circle-profile-picture-png-transparent-png.png"} name={"Sharukh"} nickname="Badhshah"/> 
+
+     <ProfileCard image={"https://www.pngitem.com/pimgs/m/404-4042710_circle-profile-picture-png-transparent-png.png"} name={"Avni"} nickname="Gungun"/> 
+
+     <ProfileCard image={"https://www.pngitem.com/pimgs/m/404-4042710_circle-profile-picture-png-transparent-png.png"} name={"Poonam"} nickname="Poornima"/> 
+
+     <ProfileCard image={"https://www.pngitem.com/pimgs/m/404-4042710_circle-profile-picture-png-transparent-png.png"} name={"Shivam"} nickname="Shiva"/> 
+
+     <ProfileCard image={"https://www.pngitem.com/pimgs/m/404-4042710_circle-profile-picture-png-transparent-png.png"} name={"Amisha"} nickname="Ammu"/> 
+    
+    
+   
+     </div>
      </>
    
         
--- 11reactproject/src/Components/ProfileCard.jsx
+++ 11reactproject/src/Components/ProfileCard.jsx
@@ -1,9 +1,31 @@
-function ProfileCard(){
+function ProfileCard(props){
+    // console.log(props)
+    console.log(props.image)
+    console.log(props.name)
     return(
         <>
-        <img src="" alt="" />
-        <h1>Welcome Poonam !! to the Vite React</h1>
+        {/* <div className="border">
+            <img className='profile-pic'src="https://www.pngitem.com/pimgs/m/404-4042710_circle-profile-picture-png-transparent-png.png" alt="" />
+            <h2>Welcome Poonam</h2>
+            <h3>NickName: XYZ</h3>
+        </div> */}
+
+        <div className="border">
+        <img className='profile-pic'src={props.image} alt="" />
+        <h2>Welcome {props.name}</h2>
+        <h3>NickName: {props.nickname}</h3>
+        </div>
         </>
     )
 }
 export default ProfileCard;
+//class for styling
+//class="" || Classes are the reserved keyword ||OOPS> class classname
+//className=""
+//params and args is js 
+//user dependent > args > recive > params> display
+//calling ut function > function()// traditions and params and args wont work.
+// html tag > calling tag
+// attribute ={"attribute value"}// args
+
+// React > Properties > Props 
