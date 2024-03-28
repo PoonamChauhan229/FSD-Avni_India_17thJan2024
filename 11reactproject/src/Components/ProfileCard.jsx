fd463d51899068dc062047cc40871760fef67b03
PoonamChauhan229 <poonam.chauhan229@gmail.com>
2024-03-28 18:48:37 +0530

    conditional rendering

--- 11reactproject/src/Components/ProfileCard.jsx
+++ 11reactproject/src/Components/ProfileCard.jsx
@@ -9,13 +9,36 @@ function ProfileCard(props){
 
     // like and disike button
     // create as an compoent
+    
+    //conditional rendering
+    // if (){//trueval}{//elsefasevalue}
+    //if else
+    //ternary operator> conditional rendering
+    // (condition)?"truevalue":"falsevale"
+    const [show,setShow]=useState(false)
     const [time,setTime]=useState(5)
-    const [btnName,setBtnName]=useState("Given")
+    const [btnName,setBtnName]=useState("")
     const myStyle={
         padding:"4px",
         color:"blue",
         backgroundColor:"pink",
         marginTop:"2px"
+    }
+    const myStyle2={
+        padding:"4px",
+        color:"blue",
+        backgroundColor:"cyan",
+        marginTop:"2px",
+        // display:"none"
+        //conditional rendering
+        display:show?"block":"none"
+        
+    }
+    const myStyle3={
+        padding:"4px",
+        color:"white",
+        backgroundColor:"green",
+        marginTop:"2px",        
     }
      return(
         <>
@@ -39,11 +62,19 @@ function ProfileCard(props){
             
             */}
             <h4 style={{color:"orange",marginBottom:"5px",marginTop:"4px"}}>NickName: {props.nickname}</h4>
-            <h4 style={myStyle}>Time {btnName}:{time} mins</h4>
+            <h4 style={myStyle2}>Time Given:5 mins</h4>
+            {show?<h5 style={myStyle}>Time {btnName}:{time} mins</h5>:null}
+
+            {/* //conditional rendering */}
+            {show && <h5 style={myStyle3}>Task Completed</h5>} {/* if(true){val} */}
+            
+
             <button style={myStyle} onClick={()=>{
                 // time+10
                 setTime(time+10)
                 setBtnName("Taken")
+                setShow(true)
+                console.log(show)
                 }}>Update Time</button>
         </div>
         </>
