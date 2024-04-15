69b9b06f9c2fb861fb89e142edb8b0fbbfbe1c10
PoonamChauhan229 <poonam.chauhan229@gmail.com>
2024-04-15 20:57:53 +0530

    reactjs

--- 11reactproject/src/App.jsx
+++ 11reactproject/src/App.jsx
@@ -5,10 +5,11 @@ import Display from './Components/Display'
 import Navbar from './Components/Navbar';
 import BootstarpEx from './Components/BootstarpEx';
 import AddMovie from './Components/AddMovie';
-import { Routes,Route } from 'react-router-dom';
+import { Navigate,Routes,Route } from 'react-router-dom';
 import MovieList from './Components/MovieList';
 import Thriller from './Components/Thriller';
 import Drama from './Components/Drama';
+import Page404 from './Components/Page404';
 function App() {
 
   return (
@@ -19,11 +20,21 @@ function App() {
       <Route path='/' element={<MovieList/>}/>
       <Route path='/thriller' element={ <Thriller/>}/> 
       <Route path='/drama' element={ <Drama/>}/> 
-      <Route path='/addcolor' element={ <AddColor/>}/> 
       <Route path='/btstrp' element={ <BootstarpEx/>}/>       
       <Route path='/reactbtstrp' element={<BasicExample/>}/>
       <Route path='/addmovieBtstrp' element={ <AddMovie/>}/>
       <Route path='/display' element={<Display/>}/> 
+
+
+      <Route path='/colorgame' element={<AddColor/>}/>
+      <Route path='/addcolor' element={<Navigate replace to ='/colorgame'/>}/>
+
+
+    {/* 404 */}
+    <Route path ='*' element={<Page404/>}/>
+    {/* /404 */}
+
+   
     </Routes>
 
      </>
@@ -34,6 +45,15 @@ function App() {
 }
 
 export default App
+
+{/* <Route path='/addcolor' element={ <AddColor/>}/>  */}
+// old route /colorgame
+// /addcolor
+// new cus + old cus
+// old cus > /addcolor
+// new cus > /colorgame
+// /addcolor >>> /colorgame
+
 
 
 
